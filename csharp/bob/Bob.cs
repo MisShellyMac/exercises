a825a52878c0620c981a0f1b26add2c3ae47c57a
Michelle McCarthy <michellemccarthy@hotmail.com>
2015-11-17 10:37:37 -0800

    Bob Tests and File for Exercism

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -6,15 +6,32 @@ using System.Threading.Tasks;
 
 namespace Exercism.bob
 {
-    class Bob
+    public class Bob
     {
-        internal static string Hey(string userphrase)
+        public static string Hey(string statement)
         {
-            if (userphrase == "Tom-ay-to, tom-aaaah-to.")
-            {
-                return "Whatever";
-            }
-                            
+            if (IsSilence(statement))
+                return "Fine. Be that way!";
+            if (IsYelling(statement))
+                return "Whoa, chill out!";
+            if (IsQuestion(statement))
+                return "Sure.";
+            return "Whatever.";
         }
-    }
-}
+
+        private static bool IsSilence(string statement)
+        {
+            return statement.Trim() == "";
+        }
+
+        private static bool IsYelling(string statement)
+        {
+            return statement.ToUpper() == statement && System.Text.RegularExpressions.Regex.IsMatch(statement, "[a-zA-Z]+");
+        }
+
+        private static bool IsQuestion(string statement)
+        {
+            return statement.Trim().EndsWith("?");
+        }
+     }
+   }
